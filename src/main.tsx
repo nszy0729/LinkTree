a3a6c5aafd31d1978999d6f2d27e982b33b2f5b2
yuta.suzuki <yuta.suzuki6130@terrasky.com>
2025-01-14 10:00:25 +0900

    Fix JSON未設定時のメッセージを追加

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { Bookmarks, Settings } from 'types';
 import Root  from './compornent';
 
 const settings: Settings = await chrome.storage.local.get('settings');
-const bookmarksJson: Bookmarks = JSON.parse(settings.settings.json || "[]");
+const bookmarksJson: Bookmarks = JSON.parse(settings?.settings?.json || "[]");
 
 const domNode = document.getElementById('root');
 if (domNode) {
